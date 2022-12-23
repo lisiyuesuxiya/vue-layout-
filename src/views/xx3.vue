b1a9a95a8aa6b3f827b116c4acc62c088f20c260
lisiyuesuxiya <1913384755@qq.com>
2022-12-23 11:32:43 +0800

    init

---
+++ src/views/xx3.vue
@@ -0,0 +1,39 @@
+<!--  -->
+<template>
+<div class=''>
+    3
+</div>
+</template>
+
+<script>
+
+export default {
+components: {},
+data() {
+return {
+
+};
+},
+computed: {},
+watch: {},
+methods: {
+
+},
+created() {
+
+},
+mounted() {
+
+},
+beforeCreate() {},
+beforeMount() {},
+beforeUpdate() {},
+updated() {},
+beforeDestroy() {},
+destroyed() {},
+activated() {},
+}
+</script>
+<style scoped>
+
+</style>
